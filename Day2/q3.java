e243d23db1dc376667a14189027f11a5d0e4ba3d
Aditya Pandey <adityapandey30273@gmail.com>
2026-07-04 15:28:57 +0530

    Add product calculation and palindrome check programs with proper scanner usage

--- Day2/q3.java
+++ Day2/q3.java
@@ -1,5 +1,21 @@
 package Day2;
-
+import java.util.*;
+// Product of a number
 public class q3 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int product =1;
+        System.out.print("Enter a number : ");
+        int num = sc.nextInt();
+        while(num>0){
+            // product = product * (num%10);
+            // num = num/10;
+        int digit = num % 10;
+        product = product * digit;
+        num = num /10;
+        }
+        System.out.println("Product of a number : " +product);
+        sc.close();
+    }
     
 }
--- Day2/q4.java
+++ Day2/q4.java
@@ -1,5 +1,29 @@
 package Day2;
-
+import java.util.*;
+//  Palindrome of a number
 public class q4 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int rev=0;
+        System.out.print("Enter a number : ");
+        int num= sc.nextInt();
+        int temp;
+        temp=num;
+        // int rem
+        while(num>0){
+             rev = rev*10 + (num%10);    // rem = num%10;
+            num = num/10;
+
+        }
+        if(temp==rev){
+            System.out.println("The number is Palindrome ");
+        }
+        else{
+            System.out.println("The number is not Palindrome");
+        }
+        sc.close();
+
+
+    }
     
 }
