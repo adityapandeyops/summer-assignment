b0bec3cb4e58f7c60d4dc7e8c9809abc6392340b
Aditya Pandey <adityapandey30273@gmail.com>
2026-07-08 17:57:10 +0530

    Add prime number checker and create empty classes for q2, q3, and q4

---
+++ Day3/q1.java
@@ -0,0 +1,33 @@
+package Day3;
+import java.util.*;
+// Prime number
+public class q1 {
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt() ;
+        // System.out.print("Enter a number");
+        // int n= sc.nextInt();
+        // for(int i=2; i<=n/2; i++){
+        //     if(n%i==0){
+        //         System.out.println("Not Prime number");
+        //     }
+        //     else 
+        //         System.out.println("Prime number");
+            
+        // }
+        //  sc.close();
+     for(int i=2; i<=n/2; i++){
+        if(n%i==0){
+            System.out.println("Not prime number");
+            return;
+        }
+        else
+            System.out.println("prime number");
+        return;
+        
+     }
+
+
+        sc.close();
+    }
+}
