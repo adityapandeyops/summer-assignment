793ba5bea23f38250182ba03ba9f27fc08330be1
Aditya Pandey <adityapandey30273@gmail.com>
2026-07-08 18:56:14 +0530

    Implement GCD calculation and ensure scanner is closed properly

--- Day3/q3.java
+++ Day3/q3.java
@@ -1,5 +1,20 @@
 package Day3;
-
+import java.util.*;
+// GCD of two number
 public class q3 {
+    public static void main(String args[]){
+        Scanner sc =new Scanner(System.in);
+        int a=sc.nextInt();
+        int b =sc.nextInt();
+        int gcd=1;
+        for(int i=1; i<=a && i<=b;i++ ){
+            if(a%i==0 && b%i==0){
+                gcd=i;
+            }
+        }
+        System.out.println(gcd);
+        sc.close();
+    }
+
     
 }
