0eb2cbf380dbbe86bcf18233b99d89a14b9d473a
Aditya Pandey <adityapandey30273@gmail.com>
2026-07-09 20:33:36 +0530

    Implement Fibonacci series calculation and ensure scanner is closed properly

--- Day4/q2.java
+++ Day4/q2.java
@@ -1,5 +1,31 @@
 package Day4;
+import java.util.*;
+//  TO find nth Fibonacci series
 
 public class q2 {
+    public static void main(String args[]){
+
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int n= sc.nextInt();
+        int a=0;
+        int b=1;
+        int c=0;
+        if(n==1){
+            System.out.println(a);
+        }
+        else if(n==2){
+System.out.println(b);
+        }
+        else {
+            for(int i=3; i<=n; i++){
+                c=a+b;
+                a=b;
+                b=c;
+            }
+            System.out.println(c);
+        }
+        sc.close();
+    }
     
 }
