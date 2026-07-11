27619f0908a71e8fa587bdf89352d9e210ff3baf
Aditya Pandey <adityapandey30273@gmail.com>
2026-07-11 19:13:22 +0530

    Implement Strong number checker in q2 class

--- Day5/q2.java
+++ Day5/q2.java
@@ -1,4 +1,27 @@
 package Day5;
+import java.util.*;
+// Strong number 
 public class q2 {
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        int temp=n, sum=0;
+        while(n>0){
+            int rem=n%10;
+            int fact=1;
+            for(int i=1; i<=rem; i++){
+                fact= fact*i;
+            }
+                sum= sum+fact;
+                n=n/10;
+        }
+        if(sum==temp){
+            System.out.println("Strong number");
+        }
+        else{
+            System.out.println("Not Strong number");
+        }
+        sc.close();
+    }
     
 }
