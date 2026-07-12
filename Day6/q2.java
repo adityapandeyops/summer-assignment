3c225724552ad6fe310c5dcd6edf4873e09b7563
Aditya Pandey <adityapandey30273@gmail.com>
2026-07-12 16:41:11 +0530

    Refactor q2 class to implement binary to decimal conversion

--- Day6/q2.java
+++ Day6/q2.java
@@ -1,5 +1,20 @@
 package Day6;
-
+import java.util.*;
+// Binary to Decimal
 public class q2 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int bin=sc.nextInt();
+        int dec=0 , base=1;
+        while(bin>0){
+            int rem= bin%10;
+            dec = dec+rem*base;
+            base = base*2;
+            bin= bin/10;
+
+        }
+        System.out.println("Decimal : " +dec);
+        sc.close();
+    }
     
 }
