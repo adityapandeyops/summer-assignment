06946031391517b3fa06a60b2570a8aa8f94ddc1
Aditya Pandey <adityapandey30273@gmail.com>
2026-07-12 16:51:23 +0530

    Refactor q3 class to count set bits in a number

--- Day6/q3.java
+++ Day6/q3.java
@@ -1,5 +1,18 @@
 package Day6;
-
+import java.util.*;
+//Program to count set bits(1's) in a number
 public class q3 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        int count=0;
+        while(n>0){
+            if(n%2==1)
+                count++;
+                n=n/2;
+        }
+        System.out.println("Set bits : " +count);
+        sc.close();
+    }
     
 }
