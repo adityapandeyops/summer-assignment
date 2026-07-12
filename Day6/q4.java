bc4b04f5759f72c653019ea23010bbd2831c774b
Aditya Pandey <adityapandey30273@gmail.com>
2026-07-13 01:21:33 +0530

    Implement main method to calculate x raised to the power of n without using pow()

--- Day6/q4.java
+++ Day6/q4.java
@@ -1,5 +1,17 @@
 package Day6;
-
+import java.util.*;
+// Program to find x^n without pow()
 public class q4 {
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        int x=sc.nextInt();
+        int n= sc.nextInt();
+        int result=1;
+        for(int i=1; i<=n; i++){
+            result = result*x;
+        }
+           System.out.println("Answer = " +result);
+           sc.close();
+    }
     
 }
