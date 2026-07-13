44996af7f0985ad3e04a6b24a0f7b957215f974c
Aditya Pandey <adityapandey30273@gmail.com>
2026-07-13 16:37:24 +0530

    Refactor q3 class to improve sum of digits calculation and enhance user interaction

--- Day7/q3.java
+++ Day7/q3.java
@@ -1,5 +1,21 @@
 package Day7;
-
+import java.util.*;
+// Recursive of Sum of Digits
 public class q3 {
-    
+    public static int sum(int num){
+        int sum=0;
+        while(num!=0){
+            sum = sum+(num%10);
+            num = num/10;
+        }
+        return sum;
+    } 
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number :");
+        int num= sc.nextInt();
+        System.out.println("Sum of digit : " +sum(num));
+        sc.close();
+    } 
+
 }
