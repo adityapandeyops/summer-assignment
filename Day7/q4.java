5259edb677ecb11dced78739ae55b4d8f6a39525
Aditya Pandey <adityapandey30273@gmail.com>
2026-07-14 11:42:11 +0530

    Refactor q4 class to implement recursive number reversal and enhance user interaction

--- Day7/q4.java
+++ Day7/q4.java
@@ -1,5 +1,19 @@
 package Day7;
-
+import java.util.*;
+//Recursive reverse number
 public class q4 {
-    
+    public static int rev=0;     //public static int reverse(int n , int rev){
+   public static int reverse(int n){
+        if(n==0)               //  if(n==0)
+         return rev;           //return rev;
+         rev = rev*10 + (n%10); //return reverse(n/10, rev*10 + n%10);}
+         return reverse (n/10); 
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int num=sc.nextInt();
+        System.out.println("Reverse : " +reverse(num));
+        sc.close();
+    }
 }
