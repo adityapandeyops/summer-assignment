2f7481850f430fe2484e244e216d9771a8ff876a
Aditya Pandey <adityapandey30273@gmail.com>
2026-07-15 01:16:21 +0530

    Implement hollow square pattern printing in q4 class

--- Day9/q4.java
+++ Day9/q4.java
@@ -1,5 +1,25 @@
 package Day9;
-
+import java.util.*;
+//  Print hollow square pattern
 public class q4 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n=5;
+        //outer loop
+        for(int i=1; i<=n; i++){
+            //inner loop
+            for(int j=1; j<=n;j++){
+                //cell => (i,j)
+                if(i==1||j==1||i==n||j==n){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
     
 }
