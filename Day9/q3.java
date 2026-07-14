548d481c736ac747450a9d6e921113bf6ff1d8a4
Aditya Pandey <adityapandey30273@gmail.com>
2026-07-14 12:26:33 +0530

    Enhance q3 class by adding character pattern printing functionality

--- Day9/q3.java
+++ Day9/q3.java
@@ -1,5 +1,22 @@
 package Day9;
-
-public class q3 {
+import java.util.*;
+// Print repeated character pattern
+public class q3 { 
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n=5;
+        // outer loop
+        char ch= 'A';
+        for(int i=1; i<=n; i++ ){
+            //inner loop
+            for(int j=1; j<=i; j++){
+                
+                System.out.print(ch+" ");
+            }
+            ch++;
+            System.out.println();
+        }
+        sc.close();
+    }
     
 }
