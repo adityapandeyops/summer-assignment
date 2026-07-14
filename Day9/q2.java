7238eab4194fdbad6d685c4887a4f957d60a4306
Aditya Pandey <adityapandey30273@gmail.com>
2026-07-14 12:11:43 +0530

    Implement reverse number triangle in q2 class

--- Day9/q2.java
+++ Day9/q2.java
@@ -1,5 +1,19 @@
 package Day9;
-
+import java.util.*;
+// Print reverse number triangle
 public class q2 {
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        int n=5;
+        //outer loop
+        for(int i=1; i<=n; i++){
+            //inner loop
+            for(int j=1; j<=n-i+1; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
     
 }
