6e69570409945920c696b11c0e7d3e77ae17dd97
Aditya Pandey <adityapandey30273@gmail.com>
2026-07-17 02:22:48 +0530

    Refactor q1.java for code style consistency and add q2.java with Armstrong number functionality

--- Day12/q2.java
+++ Day12/q2.java
@@ -1,5 +1,28 @@
 package Day12;
-
+import java.util.*;
+// write function for armstrong
 public class q2 {
+    public static int armstrong(int n){
+        int sum=0;
+        int temp = n;
+        while(temp>0){
+            int rem= temp%10;
+            sum = sum +(rem*rem*rem);
+            temp = temp/10;
+        }
+        return sum;
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int num = sc.nextInt();
+        if(num == armstrong(num)){
+             System.out.println("Armtrong number");
+        }
+        else{
+            System.out.println("Not armstrong number");
+        }
+        sc.close();
+    }
     
 }
