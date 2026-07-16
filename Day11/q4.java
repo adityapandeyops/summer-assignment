188f6e3ccd6139927ed5e89ba8a16f2f51968338
Aditya Pandey <adityapandey30273@gmail.com>
2026-07-17 00:40:36 +0530

    Implement factorial calculation in q4 class

--- Day11/q4.java
+++ Day11/q4.java
@@ -1,5 +1,21 @@
 package Day11;
-
+import java.util.*;
+// Function to find factorial
 public class q4 {
+    public static int factorial(int n){
+        int fact=1;
+        for(int i=1; i<=n; i++){
+            fact = fact*i;
+
+        }
+        return fact;
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int n= sc.nextInt();
+        System.out.println("Factorial : " +factorial(n));
+        sc.close();
+    }
     
 }
