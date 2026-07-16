b107f73ebb0ffa84320aaed1038faeb8c8e33586
Aditya Pandey <adityapandey30273@gmail.com>
2026-07-16 20:41:37 +0530

    Add functionality to find and display the maximum of two numbers in q2 class

--- Day11/q2.java
+++ Day11/q2.java
@@ -1,5 +1,19 @@
 package Day11;
-
+import java.util.*;
+// Function to find maximum
 public class q2 {
+    public static int maximum(int a, int b){
+        return (a>b)? a:b;
+    }
+    public static void main(String args[]){
+      Scanner sc= new Scanner(System.in);
+      System.out.print("Enter first number : ");
+      int a=sc.nextInt();
+      System.out.print("Enter second number : ");
+      int b=sc.nextInt();
+      int max = maximum(a, b);
+      System.out.println("Maximum : " +max);
+      sc.close();
+    }
     
 }
