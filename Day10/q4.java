79f51ed160c0b1bb09808a3f0c3c95163f3dd912
Aditya Pandey <adityapandey30273@gmail.com>
2026-07-16 20:25:20 +0530

    Implement character pyramid printing in q4 class and add q1, q2, q3, and q4 classes for Day11 exercises

--- Day10/q4.java
+++ Day10/q4.java
@@ -1,5 +1,36 @@
 package Day10;
-
+import java.util.*;
+// Program to print Character pyramid
 public class q4 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n=5;
+        // outer loop
+        for(int i=1; i<=n; i++){
+            
+            //inner loop
+            // spaces
+          for(int j=1; j<=n-i;j++){
+            System.out.print(" ");
+          }
+          char ch='A';
+          // character
+          // 1st half character
+          for(int j=1; j<=i; j++){
+            System.out.print(ch+ " ");
+            ch++;
+
+          }
+          ch -=2;
+          // 2nd half character
+          for(int j=1; j<i; j++){
+            System.out.print(ch+ " ");
+            ch--;
+          }
+          System.out.println();
+
+        }
+        sc.close();
+    }
     
 }
---
+++ Day11/q1.java
@@ -0,0 +1,20 @@
+package Day11;
+import java.util.*;
+// function to find sum of two number
+public class q1 {
+    public static int sum(int a, int b){
+        return a+b;
+    }
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter first number : ");
+        int a= sc.nextInt();
+        System.out.print("Enter second number : ");
+        int b=sc.nextInt();
+        int result = sum(a,b);
+        System.out.println("Sum = " +result);
+        sc.close();
+    }
+
+    
+}
