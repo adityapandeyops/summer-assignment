c528c2411a63f75c6ff3cc342a95ce11fe3f624a
Aditya Pandey <adityapandey30273@gmail.com>
2026-07-19 11:45:04 +0530

    Implement sum and average calculation for an array in q2.java

--- Day13/q2.java
+++ Day13/q2.java
@@ -1,5 +1,24 @@
 package Day13;
-
+import java.util.*;
+// Wap to find sum and average of array
 public class q2 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of an array : ");
+        int n= sc.nextInt();
+        int arr[]= new int[n];
+        int sum =0;
+        System.out.println("Enter array elements : ");
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+            sum = sum +arr[i];
+
+        }
+        double average = (double) sum/n;
+        System.out.println("Sum : " + sum);
+        System.out.println("Average : " + average);
+        sc.close();
+    }
     
 }
+
