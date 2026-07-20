dad046bcb4907d25a13e522101ad368cc45f3f12
Aditya Pandey <adityapandey30273@gmail.com>
2026-07-20 05:48:28 +0530

    Implement functionality to find the second largest element in an array in q3.java

--- Day14/q3.java
+++ Day14/q3.java
@@ -1,5 +1,29 @@
 package Day14;
-
+import java.util.*;
+// Wap to second largest element 
 public class q3 {
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter size of array : ");
+        int n=sc.nextInt();
+        int[] arr= new int[n];
+        System.out.println("Enter array elements : ");
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+        }
+        int largest= arr[0];
+        int secondLargest = arr[0];
+        for(int i=1; i<n; i++){
+            if(arr[i]> largest){
+                secondLargest = largest;
+                largest = arr[i];    
+            }
+            else if(arr[i]>secondLargest && arr[i] != largest){
+                secondLargest =arr[i];
+            }
+        }
+        System.out.println("Second largest element : " +secondLargest);
+        sc.close();
+    }
     
 }
