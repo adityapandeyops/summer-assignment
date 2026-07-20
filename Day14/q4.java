df7290b5b73b7646da282633f919bb20f7058df4
Aditya Pandey <adityapandey30273@gmail.com>
2026-07-20 06:01:20 +0530

    Implement functionality to find duplicate elements in an array in q4.java

--- Day14/q4.java
+++ Day14/q4.java
@@ -1,5 +1,27 @@
 package Day14;
-
+import java.util.*;
+// Wap to find duplicates in array
 public class q4 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array : ");
+        int n= sc.nextInt();
+        int[] arr= new int[n];
+        System.out.println("Enter array element : ");
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+
+        }
+        System.out.println("Duplicate elements are : ");
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                if(arr[i]==arr[j]){
+                    System.out.println(arr[i]);
+                    break;
+                }
+            }
+        }
+        sc.close();
+    }
     
 }
