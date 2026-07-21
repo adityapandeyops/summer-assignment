9ad59613f276c9249389b5edf6504537fd0fbee0
Aditya Pandey <adityapandey30273@gmail.com>
2026-07-21 19:24:58 +0530

    Implement left rotation functionality for an array in q2.java

--- Day15/q2.java
+++ Day15/q2.java
@@ -1,5 +1,26 @@
 package Day15;
-
+import java.util.*;
+// Write a program to Rotate array left. 
 public class q2 {
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter size of array :");
+        int n=sc.nextInt();
+        int[] arr= new int[n];
+        System.out.println("Enter array element :");
+        for(int i=0; i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int first = arr[0];
+        for(int i=0; i<n-1; i++){
+            arr[i]=arr[i+1];    
+        }
+        arr[n-1]=first;
+        System.out.println("Array after left rotation :");
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i] + " ");
+        }
+        sc.close();
+    }
     
 }
