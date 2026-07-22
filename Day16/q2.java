20dca4e217e1253d594fe768d8b0e7b4b0e8e341
Aditya Pandey <adityapandey30273@gmail.com>
2026-07-22 19:19:31 +0530

    Implement program to find maximum frequency element in an array

--- Day16/q2.java
+++ Day16/q2.java
@@ -1,5 +1,35 @@
 package Day16;
-
+import java.util.*;
+// Write a program to Find maximum frequency element. 
 public class q2 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array : ");
+        int n=sc.nextInt();
+        int[] arr= new int[n];
+        System.out.println("Enter array element : ");
+        for(int i=0; i<n; i++){
+            arr[i]= sc.nextInt();
+
+        }
+        int maxFreq=0;
+        int element = arr[0];
+        for(int i=0; i<n; i++){
+            int count=1;
+            for(int j=i+1; j<n; j++){
+                if(arr[i]==arr[j]){
+                    count++;
+                }
+            }
+            if(count> maxFreq){
+                maxFreq = count;
+                element =arr[i];
+            }
+        }
+        System.out.println("Maximum frequency element : " + element);
+        System.out.println("Frequency: " +maxFreq);
+        sc.close();
+
+    }
     
 }
