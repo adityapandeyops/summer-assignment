801f15cb8d888754d9198a7cd669180534d8c3b1
Aditya Pandey <adityapandey30273@gmail.com>
2026-07-23 19:48:26 +0530

    Implement program to find common elements in two arrays

--- Day17/q4.java
+++ Day17/q4.java
@@ -1,5 +1,35 @@
 package Day17;
-
+import java.util.*;
+// Write a program to Find common elements
 public class q4 {
+    public static void main(String args[]){
+        Scanner sc= new Scanner( System.in);
+        System.out.print("Enter size of first array : ");
+        int n1=sc.nextInt();
+        int[] a=new int[n1];
+        System.out.println("Enter first array element : ");
+        for(int i=0; i<n1; i++){
+            a[i]=sc.nextInt();
+
+        }
+        System.out.print("Enter size of second array : ");
+        int n2=sc.nextInt();
+        int[] b= new int[n2];
+        System.out.println("Enter second array element : ");
+        for(int i=0; i<n2;i++){
+            b[i]= sc.nextInt();
+
+        }
+        System.out.println("Common element : ");
+        for(int i=0; i<n1; i++){
+            for(int j=0; j<n2; j++){
+                if(a[i]==b[j]){
+                    System.out.print(a[i] + " ");
+                         break;
+                }
+            }
+        }
+        sc.close();
+    }
     
 }
