bad4f27d3869f88fcd31c9b7dd3cdb7f7c9fd5bc
Aditya Pandey <adityapandey30273@gmail.com>
2026-07-24 22:14:52 +0530

    Implement program to sort an array in descending order in q4.java

--- Day18/q4.java
+++ Day18/q4.java
@@ -1,5 +1,32 @@
 package Day18;
-
+import java.util.*;
+// Write a program to Sort array in descending order. 
 public class q4 {
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        System.out.println("Enter array elements: ");
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+
+        }
+        // Sorting in descending order
+        for(int i=0; i<n-1; i++){
+            for(int j=i+1; j<n; j++){
+                if(arr[i]<arr[j]){
+                    int temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+        System.out.println("Array in descending order: ");
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i] + " ");
+        }
+        sc.close();
+    }
     
 }
