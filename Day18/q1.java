ee6311f8ce01caf174ddec1cdf90866e15e30bbc
Aditya Pandey <adityapandey30273@gmail.com>
2026-07-24 21:27:36 +0530

    Implement Bubble Sort program and add initial class structures for q2, q3, and q4

---
+++ Day18/q1.java
@@ -0,0 +1,31 @@
+package Day18;
+import java.util.*;
+// Write a program to Bubble sort. 
+public class q1 {
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        System.out.println("Enter array elements: ");
+        for(int i=0; i<n; i++){
+            arr[i]= sc.nextInt();
+        }
+        // Bubble sort 
+        for(int i=0; i<n-1;i++){
+            for(int j=0; j<n-1; j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        System.out.println("Sorted array: ");
+        for(int i=0; i<n;i++){
+            System.out.print(arr[i] + " ");
+        }
+        sc.close();
+    }
+    
+}
