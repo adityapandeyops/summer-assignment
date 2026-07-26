d21e2c15050c56d5de34417cc4c36cc123e20e62
Aditya Pandey <adityapandey30273@gmail.com>
2026-07-26 16:53:15 +0530

    Implement program to calculate column-wise sum of a matrix in q4.java

--- Day20/q4.java
+++ Day20/q4.java
@@ -1,5 +1,27 @@
 package Day20;
-
+import java.util.*;
+// Write a program to Find column-wise sum. 
 public class q4 {
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n= sc.nextInt();
+        int[][] a= new int[n][n];
+        System.out.println("Enter the matrix: ");
+        for(int i=0; i<n;i++){
+            for(int j=0; j<n; j++){
+                a[i][j]=sc.nextInt();
+            }
+        }
+        System.out.println("Column-wise sum: ");
+        for(int j=0;j<n; j++){
+            int sum=0;
+            for(int i=0; i<n; i++){
+                sum= sum+a[i][j];
+            }
+            System.out.println("Sum o column" + (j + 1) + " = " +sum);
+        }
+        sc.close();
+    }
     
 }
