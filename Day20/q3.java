83c2548a622b8fc20d2dbf64063ca05546132378
Aditya Pandey <adityapandey30273@gmail.com>
2026-07-26 16:41:32 +0530

    Implement program to calculate row-wise sum of a matrix in q3.java

--- Day20/q3.java
+++ Day20/q3.java
@@ -1,5 +1,28 @@
 package Day20;
-
+import java.util.*;
+// Write a program to Find row-wise sum.
 public class q3 {
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n=sc.nextInt();
+        int[][] a = new int[n][n];
+        System.out.println("Enter the matrix: ");
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n;j++){
+                a[i][j]=sc.nextInt();
+            }
+                
+        }
+        System.out.println("Row-wise sum: ");
+        for(int i=0; i<n; i++){
+            int sum=0;
+            for(int j=0; j<n; j++){
+                sum= sum+a[i][j];
+            }
+            System.out.println("Sum of row" +(i + 1) + " = " + sum);
+        }
+        sc.close();
+    }
     
 }
