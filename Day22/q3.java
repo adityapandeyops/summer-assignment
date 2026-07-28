a953870aeff6a99cc22415d11211aa28b55ea92b
Aditya Pandey <adityapandey30273@gmail.com>
2026-07-28 20:00:18 +0530

    Implement character frequency counting program in q3.java

--- Day22/q3.java
+++ Day22/q3.java
@@ -1,4 +1,30 @@
 package Day22;
+import java.util.*;
+// Write a program to Character frequency. 
 public class q3 {
-    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        int[] freq = new int[256];
+
+        for (int i = 0; i < str.length(); i++) {
+            freq[str.charAt(i)]++;
+        }
+
+        System.out.println("Character Frequencies:");
+
+        for (int i = 0; i < 256; i++) {
+            if (freq[i] != 0) {
+                System.out.println((char)i + " = " + freq[i]);
+            }
+        }
+
+        sc.close();
+    }
 }
+    
+
