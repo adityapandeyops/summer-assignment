adfd1fc473b29c726916dc3ffbb8b68e3039121b
Aditya Pandey <adityapandey30273@gmail.com>
2026-07-28 20:04:51 +0530

    Implement program to remove spaces from a string in q4.java

--- Day22/q4.java
+++ Day22/q4.java
@@ -1,4 +1,20 @@
 package Day22;
+import java.util.*;
+// Write a program to Remove spaces from string. 
 public class q4 {
-    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        String result = str.replace(" ", "");
+
+        System.out.println("String after removing spaces: " + result);
+
+        sc.close();
+    }
 }
+    
+
