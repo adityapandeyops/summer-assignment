0bf747125367a4ae2a8d6a7e03f55432424c23ac
Aditya Pandey <adityapandey30273@gmail.com>
2026-07-29 18:38:05 +0530

    Implement program to check for anagram strings in q3.java

--- Day23/q3.java
+++ Day23/q3.java
@@ -1,5 +1,30 @@
 package Day23;
-
+import java.util.*;
+// Write a program to Check anagram strings
 public class q3 {
     
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first string: ");
+        String str1 = sc.nextLine();
+
+        System.out.print("Enter second string: ");
+        String str2 = sc.nextLine();
+
+        char[] a = str1.toLowerCase().toCharArray();
+        char[] b = str2.toLowerCase().toCharArray();
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        if (Arrays.equals(a, b)) {
+            System.out.println("Strings are Anagram");
+        } else {
+            System.out.println("Strings are not Anagram");
+        }
+        sc.close();
+    }
 }
+    
+
