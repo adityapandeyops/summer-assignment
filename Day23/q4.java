edcb8e339ee420755dd1e794492dd5108b5a265f
Aditya Pandey <adityapandey30273@gmail.com>
2026-07-29 18:42:24 +0530

    Implement program to find the maximum occurring character in a string in q4.java

--- Day23/q4.java
+++ Day23/q4.java
@@ -1,5 +1,36 @@
 package Day23;
-
+import java.util.*;
+// Write a program to Find maximum occurring character. 
 public class q4 {
     
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        int max = 0;
+        char maxChar = ' ';
+
+        for (int i = 0; i < str.length(); i++) {
+            int count = 0;
+
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(i) == str.charAt(j)) {
+                    count++;
+                }
+            }
+
+            if (count > max) {
+                max = count;
+                maxChar = str.charAt(i);
+            }
+        }
+
+        System.out.println("Maximum occurring character: " + maxChar);
+        System.out.println("Frequency: " + max);
+        sc.close();
+    }
 }
+    
+
