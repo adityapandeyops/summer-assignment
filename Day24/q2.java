0015d667cd194f618db8b26336f12e0b8578844a
Aditya Pandey <adityapandey30273@gmail.com>
2026-07-30 20:01:16 +0530

    Implement string compression program in q2.java

--- Day24/q2.java
+++ Day24/q2.java
@@ -1,5 +1,30 @@
 package Day24;
-
+import java.util.*;
+// import java.util.Scanner;
 public class q2 {
     
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        String result = "";
+        int count = 1;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)) {
+                count++;
+            } else {
+                result = result + str.charAt(i) + count;
+                count = 1;
+            }
+        }
+
+        System.out.println("Compressed String: " + result);
+
+        sc.close();
+    }
 }
+    
+
