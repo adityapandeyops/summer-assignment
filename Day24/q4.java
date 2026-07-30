5e7ef5a9d7d5f13b6b99e64f19decd5a9a6dbb7d
Aditya Pandey <adityapandey30273@gmail.com>
2026-07-30 20:10:01 +0530

    Implement program to remove duplicate characters from a string in q4.java

--- Day24/q4.java
+++ Day24/q4.java
@@ -1,5 +1,28 @@
 package Day24;
-
+import java.util.*;
+// Wap to remove duplicate character.
 public class q4 {
     
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        String result = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (result.indexOf(ch) == -1) {
+                result += ch;
+            }
+        }
+
+        System.out.println("String after removing duplicates: " + result);
+
+        sc.close();
+    }
 }
+    
+
