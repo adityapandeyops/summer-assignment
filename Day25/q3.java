0d26e5ff9f1c094ca39ced107b6bcfe3c3881fbb
Aditya Pandey <adityapandey30273@gmail.com>
2026-07-31 23:03:04 +0530

    Implement program to sort names alphabetically in q3.java

--- Day25/q3.java
+++ Day25/q3.java
@@ -1,5 +1,30 @@
 package Day25;
-
+import java.util.*;
+// Wap to sort names alphabetically.
 public class q3 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of names: ");
+        int n = sc.nextInt();
+        sc.nextLine(); // Consume newline
+
+        String[] names = new String[n];
+
+        System.out.println("Enter names:");
+        for (int i = 0; i < n; i++) {
+            names[i] = sc.nextLine();
+        }
+
+        Arrays.sort(names);
+
+        System.out.println("Names in alphabetical order:");
+        for (int i = 0; i < n; i++) {
+            System.out.println(names[i]);
+        }
+
+        sc.close();
+    }
 }
+    
+
