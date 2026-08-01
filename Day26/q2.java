bdf94c74775004a6c68bf0f10485e1bec532ba5f
Aditya Pandey <adityapandey30273@gmail.com>
2026-08-01 18:41:17 +0530

    Implement voting eligibility system in q2.java

--- Day26/q2.java
+++ Day26/q2.java
@@ -1,5 +1,22 @@
 package Day26;
-
+import java.util.*;
+// Wap to create voting eligibility system.
 public class q2 {
     
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+
+        if (age >= 18) {
+            System.out.println("You are eligible to vote.");
+        } else {
+            System.out.println("You are not eligible to vote.");
+        }
+
+        sc.close();
+    }
 }
+    
+
