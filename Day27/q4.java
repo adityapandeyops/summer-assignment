f2580e4075b35d77f2c260f9e42672f6ca3ed74f
Aditya Pandey <adityapandey30273@gmail.com>
2026-08-02 15:39:16 +0530

    Implement marksheet generation system in q4.java

--- Day27/q4.java
+++ Day27/q4.java
@@ -1,5 +1,36 @@
 package Day27;
-
+import java.util.*;
+// Wap to create marksheet generation system.
 public class q4 {
-    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Student Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter Marks of 5 Subjects: ");
+        int s1 = sc.nextInt();
+        int s2 = sc.nextInt();
+        int s3 = sc.nextInt();
+        int s4 = sc.nextInt();
+        int s5 = sc.nextInt();
+
+        int total = s1 + s2 + s3 + s4 + s5;
+        double percentage = total / 5.0;
+
+        System.out.println("\n----- Marksheet -----");
+        System.out.println("Name: " + name);
+        System.out.println("Total Marks: " + total);
+        System.out.println("Percentage: " + percentage + "%");
+
+        if (percentage >= 40)
+            System.out.println("Result: Pass");
+        else
+            System.out.println("Result: Fail");
+
+        sc.close();
+    }
 }
+    
+
