b6a4c70826d45960d15acfc19bcf5985b69450f0
Aditya Pandey <adityapandey30273@gmail.com>
2026-08-02 15:34:35 +0530

    Implement salary management system in q3.java

--- Day27/q3.java
+++ Day27/q3.java
@@ -1,5 +1,30 @@
 package Day27;
-
+import java.util.*;
+// Wap to create salary management system.
 public class q3 {
     
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Employee Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter Basic Salary: ");
+        double basic = sc.nextDouble();
+
+        double hra = basic * 0.20;
+        double da = basic * 0.10;
+        double total = basic + hra + da;
+
+        System.out.println("\nSalary Details");
+        System.out.println("Name: " + name);
+        System.out.println("Basic Salary: " + basic);
+        System.out.println("HRA: " + hra);
+        System.out.println("DA: " + da);
+        System.out.println("Total Salary: " + total);
+
+        sc.close();
+    }
 }
+    
+
