8f3bb737298cdb30e07ddb04a27bc2e3e64fcd36
Aditya Pandey <adityapandey30273@gmail.com>
2026-08-02 15:29:43 +0530

    Implement employee management system in q2.java

--- Day27/q2.java
+++ Day27/q2.java
@@ -1,5 +1,37 @@
 package Day27;
-
+import java.util.*;
+// Wap to create employee management system.
 public class q2 {
     
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] id = new int[10];
+        String[] name = new String[10];
+        double[] salary = new double[10];
+
+        System.out.print("Enter number of employees: ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter Employee ID: ");
+            id[i] = sc.nextInt();
+            sc.nextLine();
+
+            System.out.print("Enter Name: ");
+            name[i] = sc.nextLine();
+
+            System.out.print("Enter Salary: ");
+            salary[i] = sc.nextDouble();
+        }
+
+        System.out.println("\nEmployee Records");
+        for (int i = 0; i < n; i++) {
+            System.out.println(id[i] + " " + name[i] + " " + salary[i]);
+        }
+
+        sc.close();
+    }
 }
+    
+
