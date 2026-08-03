d0c2f5653b7a0e8c8ddf19d8c0c39cf54a04d487
Aditya Pandey <adityapandey30273@gmail.com>
2026-08-03 18:20:49 +0530

    Implement bank account system in q2.java

--- Day28/q2.java
+++ Day28/q2.java
@@ -1,5 +1,32 @@
 package Day28;
-
+import java.util.*;
+// Wap to create bank account system.
 public class q2 {
     
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int balance = 1000;
+
+        System.out.println("Balance = " + balance);
+
+        System.out.print("Enter Deposit Amount: ");
+        int deposit = sc.nextInt();
+        balance = balance + deposit;
+        System.out.println("Balance = " + balance);
+
+        System.out.print("Enter Withdraw Amount: ");
+        int withdraw = sc.nextInt();
+
+        if (withdraw <= balance) {
+            balance = balance - withdraw;
+            System.out.println("Final Balance = " + balance);
+        } else {
+            System.out.println("Insufficient Balance");
+        }
+
+        sc.close();
+    }
 }
+    
+
