51c4ed587eb052f249902230616ad4271987c218
Aditya Pandey <adityapandey30273@gmail.com>
2026-08-03 18:27:29 +0530

    Implement contact management system in q4.java

--- Day28/q4.java
+++ Day28/q4.java
@@ -1,5 +1,25 @@
 package Day28;
-
+import java.util.*;
+// Wap to create contact management system.
 public class q4 {
     
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String name, phone;
+
+        System.out.print("Enter Contact Name: ");
+        name = sc.nextLine();
+
+        System.out.print("Enter Phone Number: ");
+        phone = sc.nextLine();
+
+        System.out.println("\nContact Saved Successfully!");
+        System.out.println("Name : " + name);
+        System.out.println("Phone: " + phone);
+
+        sc.close();
+    }
 }
+    
+
