941be807556e4de5c4c94e4a2f908b63a1fb3c46
Aditya Pandey <adityapandey30273@gmail.com>
2026-08-03 18:24:09 +0530

    Implement ticket booking system in q3.java

--- Day28/q3.java
+++ Day28/q3.java
@@ -1,5 +1,25 @@
 package Day28;
-
+import java.util.*;
+// Wap to create ticket booking system.
 public class q3 {
-    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int seats = 10;
+
+        System.out.print("Enter number of tickets: ");
+        int tickets = sc.nextInt();
+
+        if (tickets <= seats) {
+            seats = seats - tickets;
+            System.out.println("Ticket Booked Successfully.");
+            System.out.println("Remaining Seats = " + seats);
+        } else {
+            System.out.println("Tickets Not Available.");
+        }
+
+        sc.close();
+    }
 }
+
