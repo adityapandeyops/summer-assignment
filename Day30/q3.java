4d1b5b998e1f8df6d8b4eb61314d26a9442d4bce
Aditya Pandey <adityapandey30273@gmail.com>
2026-08-05 17:32:42 +0530

    Implement mini employee management system in q3.java

--- Day30/q3.java
+++ Day30/q3.java
@@ -1,5 +1,34 @@
 package Day30;
-
+import java.util.*;
+// Wap to create mini employee management system.
 public class q3 {
     
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] id = new int[3];
+        String[] name = new String[3];
+
+        // Input employee details
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Enter Employee " + (i + 1) + " Details");
+            System.out.print("ID: ");
+            id[i] = sc.nextInt();
+
+            System.out.print("Name: ");
+            name[i] = sc.next();
+        }
+
+        // Display employee details
+        System.out.println("\nEmployee Records");
+        System.out.println("ID\tName");
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(id[i] + "\t" + name[i]);
+        }
+
+        sc.close();
+    }
 }
+    
+
